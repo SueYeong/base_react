6afb42128e5c11b34db4ec0b2eccf0a61d464157
SueYeong <dudtb205@gmail.com>
2022-06-15 09:44:06 +0900

    props rule

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ const App = () => {
       <FnComponent_2 /> */}
       {/* <ClassComponent /> */}
 
-      <FoodMenu />
+      {/* *props
+      =><컴포넌트 프롭스명="값"/> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" side="사이다" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="에슐리" side="제로콜라" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="닭가슴살" side="물" />
+      {/* 2일차 */}
     </div>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,12 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴는 텐동</h3>;
+export const FoodMenu = ({ menuName, side }) => {
+  // console.log(porps);
+  // =>프롭스는 오브젝트형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할것
+  // console.log(menuName);
+  // console.log(side);
+  return (
+    <div>
+      오늘의 메뉴는 <i>{menuName}</i> 사이드 메뉴는 <i>{side}</i>
+      {/* =>jsx안쪽에 변수를 사용할시 중괄호로 감싸줄것 */}
+    </div>
+  );
 };
