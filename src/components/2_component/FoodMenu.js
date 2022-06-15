e3d9f92206cf78a7f0c79c62bf5c207745d944cc
SueYeong <dudtb205@gmail.com>
2022-06-15 09:56:49 +0900

    jsx styles

--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,5 @@
+import "../../styles/styles.css";
+
 export const FoodMenu = ({ menuName, side }) => {
   // console.log(porps);
   // =>프롭스는 오브젝트형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할것
@@ -5,7 +7,8 @@ export const FoodMenu = ({ menuName, side }) => {
   // console.log(side);
   return (
     <div>
-      오늘의 메뉴는 <i>{menuName}</i> 사이드 메뉴는 <i>{side}</i>
+      오늘의 메뉴는 <i className="menu">{menuName}</i> 사이드 메뉴는{" "}
+      <i className="side">{side}</i>
       {/* =>jsx안쪽에 변수를 사용할시 중괄호로 감싸줄것 */}
     </div>
   );
